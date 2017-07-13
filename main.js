d347d960fea057977c3ad649531779dd1528135a
Aaron Abbott <aabmass@gmail.com>
2017-07-13 12:42:53 -0400

    Changed params, added easing

--- main.js
+++ main.js
@@ -14,9 +14,9 @@
     || document.body.clientHeight;
 
   // some other static calculations
-  var scrollBorderWidth = 0.20;
-
+  var scrollBorderWidth = 0.15;
   var scrollPercent = 0.05;
+  var scrollDurationMs = 200;
   var xScrollOffset = width * scrollPercent;
   var yScrollOffset = height * scrollPercent;
 
@@ -72,8 +72,48 @@
 
     // finally cause the window to scroll
     if (xScroll || yScroll) {
-      scrollBy(xScroll, yScroll);
+      scrollBySmooth(xScroll, yScroll, scrollDurationMs);
     }
+  };
+
+  /** 
+   * Easing function for smoothing the scrolling
+   */
+  var easeOutCubic = function (t) { return (--t)*t*t+1 };
+  var easeOutQuint = function (t) { return 1+(--t)*t*t*t*t };
+  var isCurrentlyScrolling = false;
+
+  var scrollBySmooth = function(dx, dy, duration, easeingFunc) {
+    // only allow one scrollBySmooth to run at a time
+    if (isCurrentlyScrolling) return;
+
+    easeingFunc = easeingFunc || easeOutQuint;
+    isCurrentlyScrolling = true;
+    var start = null;
+    var xStart = window.scrollX;
+    var yStart = window.scrollY;
+
+    var animate = function(time) {
+      if (!start) start = time;
+      var tPerc = (time - start) / duration;
+
+      // calculate next position
+      var easePerc = easeingFunc(tPerc);
+      var xNext = easePerc * dx + xStart;
+      var yNext = easePerc * dy + yStart;
+
+      // scroll to next
+      window.scrollTo(xNext, yNext);
+
+      if (tPerc < 1) {
+        window.requestAnimationFrame(animate);
+      }
+      else {
+        isCurrentlyScrolling = false;
+      }
+    };
+
+    window.requestAnimationFrame(animate);
   };
               
 }(window));
